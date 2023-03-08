2d61bebf84d46fe56ce65fac6beba27afaf3b16b
lenagend <lenagend@naver.com>
2023-03-08 16:23:17 +0900

    Initial commit

---
+++ src/main/java/com/kkm/talkbytag/TalkByTagApplication.java
@@ -0,0 +1,13 @@
+package com.kkm.talkbytag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkByTagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkByTagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kkm/talkbytag/TalkByTagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkm.talkbytag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkByTagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
