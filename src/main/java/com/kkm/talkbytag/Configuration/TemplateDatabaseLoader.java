d43fc3a0d0512810a2e7b1f6319050da0aa12d7a
lenagend <lenagend@naver.com>
2023-03-23 16:46:21 +0900

    2023-03-23 [change databaseLoader]

--- src/main/java/com/kkm/talkbytag/Configuration/TemplateDatabaseLoader.java
+++ src/main/java/com/kkm/talkbytag/Configuration/TemplateDatabaseLoader.java
@@ -1,20 +1,23 @@
-//package com.kkm.talkbytag.Configuration;
-//
-//import com.kkm.talkbytag.domain.Post;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.data.mongodb.core.MongoOperations;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class TemplateDatabaseLoader {
-//    @Bean
-//    CommandLineRunner initialize(MongoOperations mongo) {
-//        return args -> {
-//            for (int i = 0; i < 100; i++){
-//                mongo.save(new Post( "freeTalk", "user1",  "contents" + i ));
-//
-//            }
-//        };
-//    }
-//}
+package com.kkm.talkbytag.Configuration;
+
+import com.kkm.talkbytag.domain.Post;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class TemplateDatabaseLoader {
+    @Bean
+    CommandLineRunner initialize(MongoOperations mongo) {
+        return args -> {
+            for (int i = 0; i < 50; i++){
+                mongo.save(new Post("" + i, "hashTag" + i, "testUser" + i, "testContents" + i, LocalDateTime.now(), LocalDateTime.now(), 0, 0, true, 0));
+                Thread.sleep(10); // 0.1초 지연
+            }
+        };
+    }
+}
