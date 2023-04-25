bc9ff08192af82d4a4cc5a5e0f3af1a57839a513
lenagend <lenagend@naver.com>
2023-04-25 17:43:58 +0900

    2023-04-25 [초기데이터 저장 변경]

--- src/main/java/com/kkm/talkbytag/Configuration/TemplateDatabaseLoader.java
+++ src/main/java/com/kkm/talkbytag/Configuration/TemplateDatabaseLoader.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.Collections;
 
 @Component
@@ -16,9 +17,12 @@ public class TemplateDatabaseLoader {
     @Bean
     CommandLineRunner initialize(MongoOperations mongo) {
         return args -> {
+            LocalDateTime baseDate = LocalDateTime.of(2023, Month.APRIL, 24, 0, 0);
+
             for (int i = 0; i < 100; i++){
-                mongo.save(new Post("" + i, "제목" + i, "testuser1@gmail.com", "testContents" + i, LocalDateTime.now(), LocalDateTime.now(), true, 0));
-                Thread.sleep(10); // 0.1초 지연
+                LocalDateTime createdAt = baseDate.plusMinutes(i * 10);
+                LocalDateTime modifiedAt = createdAt;
+                mongo.save(new Post("" + i, "제목" + i, "testuser1@gmail.com", "testContents" + i, createdAt, modifiedAt, true, 0));
             }
             mongo.save(new User("testuser1@gmail.com", "password1", Collections.emptyList()));
             mongo.save(new UserInfo("userInfo1", "testuser1@gmail.com", "운영자", "/images/avatar/master.jpg", null, null, null, null));
